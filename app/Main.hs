8a2f3e80c8deaafafc81feae040b976061b0b6c2
Tomasz Batko <batkot@protonmail.com>
2017-07-07 20:25:25 +0200

    Add -Wall -Werror to stack.yaml

--- app/Main.hs
+++ app/Main.hs
@@ -9,26 +9,26 @@ cCompile :: IO()
 cCompile = compile . C.transpileToC $ C.C "ptr" "data"
 
 compile :: C.Compilator -> IO()
-compile compilator = getContents >>= run
+compile compilator = getContents >>= runCompilation
   where
-    run = mapM_ putStrLn . compilationPipeline compilator
+    runCompilation = mapM_ putStrLn . compilationPipeline compilator
 
 compilationPipeline :: C.Compilator -> String -> [String]
-compilationPipeline compilator bf = either printErrors compile . P.parse . L.tokenize $ bf
+compilationPipeline compilator bf = either printErrors doCompile . P.parse . L.tokenize $ bf
   where
     printErrors = fmap show
-    compile = compilator . P.optimize
+    doCompile = compilator . P.optimize
 
 run :: IO()
 run = do
   bf <- getLine
-  d <- either printErrors run . P.parse . L.tokenize $ bf
+  d <- either printErrors doRun . P.parse . L.tokenize $ bf
   putStrLn d
   main
   where
     zeroData = R.initData 0
     printErrors x = return . concat . fmap show $ x
-    run = fmap show . R.runProgram zeroData . P.optimize 
+    doRun = fmap show . R.runProgram zeroData . P.optimize 
 
 main :: IO()
 main = cCompile
