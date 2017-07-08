e31b94875465dcd617cd138e3fcb356e7a2f0cb2
Tomasz Batko <batkot@protonmail.com>
2017-07-08 21:10:44 +0200

    Add command line handling
    - optparse-applicative integration

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,71 @@
 module Main where
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+import Control.Monad (sequence)
+
 import qualified Lexer as L
 import qualified Parser as P
 import qualified Runtime as R
 import qualified Compile as C
+
+data BfcOptions = BfcOptions 
+                { bfcCommand :: BfcCommand
+                } deriving Show
+
+data BfcCommand = Compile CompileOptions
+                | Run RunOptions
+                deriving Show
+
+data CompileOptions = CompileOptions
+                    { filesToCompile :: [String]
+                    , output :: String } deriving Show
+
+data RunOptions = RunOptions
+                { files :: [String]
+                , mode :: RunMode } deriving Show
+
+data RunMode = RunMode
+             | ReplMode 
+             deriving Show
+
+bfcOptionsParser :: Parser BfcOptions
+bfcOptionsParser = BfcOptions 
+                <$> subparser
+                    ( command "compile" (info compileParser ( progDesc "Compiles Brainfuck sources to given language" ))
+                   <> command "run" (info runCmdParser ( progDesc "Runs Brainfuck sources" ))
+                    )
+
+compileParser :: Parser BfcCommand
+compileParser = Compile <$> compileOptionsParser
+
+compileOptionsParser :: Parser CompileOptions
+compileOptionsParser = CompileOptions
+                    <$> some (argument str
+                               (metavar "INPUT_SOURCES"
+                               <> help "Brainfuck sources to compile")
+                             )
+                    <*> strOption
+                        ( long "output"
+                        <> metavar "OUTPUT_FILE"
+                        <> showDefault
+                        <> value "out"
+                        <> help "Output file name")
+
+runCmdParser :: Parser BfcCommand
+runCmdParser = Run <$> runOptionsParser
+
+runOptionsParser :: Parser RunOptions
+runOptionsParser = RunOptions
+                <$> many (argument str
+                          (metavar "INPUT_SOURCES"
+                          <> help "Brainfuck sources to be run")
+                         )
+                <*> flag RunMode ReplMode
+                    ( long "interactive"
+                    <> short 'i'
+                    <> help "starts REPL"
+                    )
 
 cCompile :: IO()
 cCompile = compile . C.transpileToC $ C.C "ptr" "data"
@@ -30,5 +92,20 @@ run = do
     printErrors x = return . concat . fmap show $ x
     doRun = fmap show . R.runProgram zeroData . P.optimize 
 
+run' :: RunOptions -> IO()
+run' (RunOptions fs _) = do
+    contents <- sequence . fmap readFile $ fs
+    putStrLn . concat . fmap show . fmap (P.parse . L.tokenize ) $ contents
+
+compile' :: CompileOptions -> IO()
+compile' _ = putStrLn "Hello from compile"
+
 main :: IO()
-main = cCompile
+main = execParser opts >>= dispatch
+  where
+    opts = info (bfcOptionsParser <**> helper)
+      ( fullDesc
+      <> progDesc "Description of this sheeeeit"
+      <> header "The _ Brainfuck Compilation System")
+    dispatch (BfcOptions (Run r)) = run' r
+    dispatch (BfcOptions (Compile c)) = compile' c
